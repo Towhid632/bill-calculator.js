b162a7986edbd7d0e9f0c6449bff981d4271963f
Towhid632 <113722832+Towhid632@users.noreply.github.com>
2024-08-23 19:17:29 +0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,6 +1,1 @@
-const hide_result = document.querySelector(".HideButton");
 
-hide_result.addEventListener("click", ()=>{
-    let result = document.querySelector(".result");
-    result.style.display = "none";
-})
